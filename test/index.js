a274a6471691d500884af6e0444d21559a8c12db
Jan Eckert <jan.eckert@tum.de>
2020-11-30 19:25:26 +0100

    added test for cyclic read

--- test/index.js
+++ test/index.js
@@ -4,11 +4,11 @@ const shoom = require("..");
 test("can create, open, write and read to shared memory", function (t) {
     const server = new shoom.Shm({path: "shoom_test", size: 4});
     server.create();
-    server.write(0, new Buffer([1, 2, 3, 4]));
+    server.write(0, Buffer.from([1, 2, 3, 4]));
 
     const client = new shoom.Shm({path: "shoom_test", size: 4});
     client.open();
-    const b = new Buffer(4);
+    const b = Buffer.alloc(4);
     client.read(0, b);
 
     t.equal(b[0], 1);
@@ -18,6 +18,25 @@ test("can create, open, write and read to shared memory", function (t) {
 
     t.end();
 })
+
+test("buffer works in cycle-mode", function (t) {
+    const server = new shoom.Shm({path: "shoom_test", size: 4});
+    server.create();
+    server.write(0, Buffer.from([1, 2, 3, 4]));
+
+    const client = new shoom.Shm({path: "shoom_test", size: 4});
+    client.open();
+    const b = Buffer.alloc(4);
+    client.readCyclic(2, b);
+
+    t.equal(b[0], 3);
+    t.equal(b[1], 4);
+    t.equal(b[2], 1);
+    t.equal(b[3], 2);
+
+    t.end();
+})
+
 
 test("cannot open non-existent shared memory", function (t) {
     const client = new shoom.Shm({path: "shoom_woops", size: 4});
